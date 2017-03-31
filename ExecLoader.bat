7bac7b42e55fa2e9cc2ca871d66a6dcfc82b4a6d
Andre Issa <andrelouisissa@gmail.com>
2017-03-31 21:22:29 +1100

    Autorun generator and extension trimmer
    'Installs' console executables to the Binaries folder and compiles an Autorun file

---
+++ ExecLoader.bat
@@ -0,0 +1,22 @@
+:: 'Installs' console executables to the Binaries folder and compiles an Autorun file
+:: Place this in a new folder inside Binaries
+:: Run once, then put console exceutables into the Data folder and run again
+
+@echo off
+SET DAT=%~dp0%Data
+SET BUF=%~dp0%Buffer
+mkdir "%DAT%"
+mkdir "%BUF%"
+cd "%~dp0"
+cd ..
+SET INS=%cd%
+xcopy "%DAT%" "%BUF%" /y
+cd %BUF%
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /f "tokens=*" %%f in ('dir /b *.*') do (
+  SET newname=%%~nf
+  echo "exec !newname!" >>Autorun
+  move "%%f" "!newname!"
+)
+xcopy "%BUF%" "%INS%" /y
+del *.* /Q
